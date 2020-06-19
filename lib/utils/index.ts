7c91cad080e73157af10576e8b45b34c38f83ef1
Ofir Genish <ofir3322@gmail.com>
2020-06-19 16:17:53 +0300

    create an object validation definitions logic; create an array validation definitions logic;

--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -8,3 +8,32 @@ export function errorToJson(err: any) {
 
     return alt;
 }
+
+
+export function objectOwnEveryProperties(obj: object, properties: Array<string>) {
+    return typeof obj == 'object' && properties.every(p => (obj as Object).hasOwnProperty(p)) 
+}
+
+export function objectOwnSomeProperties(obj: object, properties: Array<string>) {
+    return typeof obj == 'object' && properties.some(p => (obj as Object).hasOwnProperty(p)) 
+}
+
+
+export function objectOwnNoProperties(obj: object, properties: Array<string>) {
+    return typeof obj == 'object' && properties.every(p => !(obj as Object).hasOwnProperty(p)) 
+}
+
+
+
+
+
+export function arrayOfType(arr: Array<any>, type: string) {
+    return Array.isArray(arr) && arr.every(v => typeof v == type);
+}
+
+export function arrayOfLength(arr: Array<any>, len: number) {
+    return Array.isArray(arr) && arr.length == len;
+}
+
+
+
